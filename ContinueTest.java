5d959b7158247901c7553cd6b2dd10000556110b
Victoria Brown <victoriabrown0710@gmail.com>
2015-10-02 11:28:07 -0500

    Initial Commit

---
+++ ContinueTest.java
@@ -0,0 +1,14 @@
+// ContinueTest.java 10/2/15
+// continue statement terminating an iteration of a for statement.
+public class ContinueTest {
+	public static void main(String[] args) {
+		for (int count = 1; count <= 10; count++) { // loop 10 times
+			if (count == 5) {
+				continue; }  // skip remaining code in loop body if count is 5
+				
+			System.out.printf("%d ", count);
+			}
+			
+			System.out.printf("%nUsed continue to skip printing 5%n");
+		}
+	}
